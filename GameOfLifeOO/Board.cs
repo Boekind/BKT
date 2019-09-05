a862744a945878c219b9665640ea2e8c75e96e06
Julia Kordes <julia.kordes@viega.de>
2019-09-05 14:16:36 +0200

    -Klassendiagramm vervollständigt
     -Aggregationen hinzugefügt

--- GameOfLifeOO/Board.cs
+++ GameOfLifeOO/Board.cs
@@ -6,5 +6,9 @@ namespace GameOfLifeOO
 {
     class Board
     {
+        public Board()
+        {
+
+        }
     }
 }
--- GameOfLifeOO/Program.cs
+++ GameOfLifeOO/Program.cs
@@ -4,9 +4,17 @@ namespace GameOfLifeOO
 {
     class Program
     {
+        //Board board;
+
+        //public Program()
+        //{
+        //    board = new Board();
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Program.Instance.Start();
+            //new Program().Start();
         }
     }
 }

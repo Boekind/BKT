7e0fa48b3ed02081d145ac3185910bc983c3d59c
Kordes <julia.kordes@viega.de>
2020-06-22 07:50:34 +0200

    Tested dynamic grids

--- BKT/WpfTest/MainWindow.xaml.cs
+++ BKT/WpfTest/MainWindow.xaml.cs
@@ -25,31 +25,44 @@ namespace WpfTest
 
         public MainWindow()
         {
-            
+
+            for (int i = 0; i < 4; i++)
+            {
+                Label lb = new Label();
+                lb.Name = $"label{i}";
+                lb.Content = i * 2;
+                lb.Width = 20;
+                //https://www.c-sharpcorner.com/UploadFile/mahesh/grid-in-wpf/
+                myGrid.Children.Add(lb);
+            }
         }
 
-        private void Test(object sender, RoutedEventArgs e, Button button)
-        {
-            Console.WriteLine($"Klick {button.Content}!");
-            Console.WriteLine();
-        }
+        //private void Test(object sender, RoutedEventArgs e, Button button)
+        //{
+        //    Console.WriteLine($"Klick {button.Content}!");
+        //    Console.WriteLine();
+        //}
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            Test(sender, e, Button1);
-            Button2.IsEnabled = true;
-        }
+        //private void Button1_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Test(sender, e, Button1);
+        //    Button2.IsEnabled = true;
+        //    labelFocus.Background = Brushes.Red;
+        //    //Styles?
+        //}
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
-        {
-            Test(sender, e, Button2);
-        }
+        //private void Button2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Test(sender, e, Button2);
+        //    labelFocus.Background = Brushes.Blue;
 
-        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            //Console.WriteLine("Test");
-            //Console.WriteLine(sender);
-        
-        }
+        //}
+
+        //private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    var label = sender as Label;
+        //    labelFocus = label;
+
+        //}
     }
 }
